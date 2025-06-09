eb483102cd9a143b0368917237274989c1a149b9
Manar Ahmad <manara16@yahoo.com>
2025-06-09 22:51:57 +0200

    Fix Navbar Menu

--- src/app/components/header/nav-bar/nav-bar.component.ts
+++ src/app/components/header/nav-bar/nav-bar.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
   activeRoute: string = '';
 
-  constructor(private router: Router) {
+  @ViewChild('toggleButton') toggleButton: ElementRef;
+
+  constructor(private router: Router, private renderer: Renderer2) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.activeRoute = event.url;
       }
     });
+
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (
+        document
+          .getElementById('navbarSupportedContent')!
+          .classList.contains('show')
+      ) {
+        this.toggleButton.nativeElement.click();
+      }
+    });
   }
+
   isActive(route: string): boolean {
     return this.activeRoute === route;
   }
